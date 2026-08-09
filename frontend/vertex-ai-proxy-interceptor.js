b33eeb4f6369f59410924fbcb7b3adaba9a229c3
Bezarate <161032613+mrbezarate@users.noreply.github.com>
2026-08-09 12:32:55 +0500

    /

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'MbYu1J0pnWWmx15DQkesI8Bucy5U4-ne',
+            'X-App-Proxy': 'T8FAnI5CBH0-vwKGFRu6fXKMImsOsHyC',
           },
           body: JSON.stringify(requestDetails),
         };
